3b95b199538721d5cb484c775a9895e9cbe4125d
amrizjantan <manager@edgedept.com>
2022-01-26 18:27:17 +0100

    Index.js updated

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.get('/documentation', (req, res) => {
   
 
 //return json to get all movies
-  app.get('/movies', (req, res) => {
+  app.get('/movies', passport.authenticate('jwt', {session:false}),(req, res) => {
     Movies.find()
      .then((movies) => {
         res.status(201).json(movies);
@@ -83,7 +83,7 @@ app.get('/documentation', (req, res) => {
   });
 
 //return json to get all users
-  app.get('/users', (req, res) => {
+  app.get('/users', passport.authenticate('jwt', {session:false}),(req, res) => {
     Users.find()
       .then((users) => {
         res.status(201).json(users);

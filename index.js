f4dcbb99bbce4f3e1160b355fe5d95238b8234c8
amrizjantan <manager@edgedept.com>
2022-01-26 18:05:12 +0100

    update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.get('/documentation', (req, res) => {
   
 
 //return json to get all movies
-  app.get('/movies',passport.authenticate('jwt', {session:false}), (req, res) => {
+  app.get('/movies', (req, res) => {
     Movies.find()
      .then((movies) => {
         res.status(201).json(movies);

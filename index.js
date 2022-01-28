1b2d72d9f82cb9dee3dd58b16cd193d1a23d7b3d
amrizjantan <manager@edgedept.com>
2022-01-28 12:35:38 +0100

    update index.js

--- index.js
+++ index.js
@@ -56,13 +56,13 @@ const uuid = require ('uuid');
 
 const { title } = require('process');
 
-const userData = {
-  _id:Users.ObjectId,
-  Username: Users.Username,
-  Email: Users.Email,
-  Birthday: Users.Birthday,
-  FavoriteMovies : Movies.ObjectId
-}
+//const userData = {
+ // _id:Users.ObjectId,
+ // Username: Users.Username,
+  //Email: Users.Email,
+  //Birthday: Users.Birthday,
+  //FavoriteMovies : Movies.ObjectId
+//}
 
 
 //App GET
@@ -114,17 +114,36 @@ app.get('/movies/:Title',passport.authenticate('jwt', {session:false}), (req, re
 
 
 // Get JSON user by username
+//app.get('/users/:Username',passport.authenticate('jwt', {session:false}),(req, res) => {
+  //Users.findOne({ Username: req.params.Username })
+   // .then((userData) => {
+    //  res.json(userData);
+    //})
+    //.catch((err) => {
+     // console.error(err);
+    //  res.status(500).send('Error: ' + err);
+   // });
+//});
+
+
+// Get JSON user by username
 app.get('/users/:Username',passport.authenticate('jwt', {session:false}),(req, res) => {
   Users.findOne({ Username: req.params.Username })
-    .then((userData) => {
-      res.json(userData);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    });
-});
-
+  .then((user) => {
+  const userData = {
+  _id: user._Id,
+  Username: user.Username,
+  Email: user.Email,
+  Birthday: user.Birthday,
+  FavoriteMovies : user.FavoriteMovies
+  }
+  res.json(userData);
+  })
+  .catch((err) => {
+  console.error(err);
+  res.status(500).send('Error: ' + err);
+  });
+  });
 
 // GET data about a genre (description) 
 app.get("/genres/:genre",passport.authenticate('jwt', {session:false}), (req, res) => {

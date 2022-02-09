088a891d13c6ca70546ace1605b4a10d66b068f5
amrizjantan <manager@edgedept.com>
2022-02-09 19:06:21 +0100

    CORS local host 1234 added

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //use CORS
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
 
 app.use(cors({
   origin: (origin, callback) => {

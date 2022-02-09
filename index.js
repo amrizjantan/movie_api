a44e35b350c4d4fdd97d1bb46ea528c0b3a6f40a
amrizjantan <manager@edgedept.com>
2022-02-09 18:40:54 +0100

    passport removed temporarily

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.get('/documentation', (req, res) => {
   
 
 //return json to get all movies
-  app.get('/movies', passport.authenticate('jwt', {session:false}),(req, res) => {
+  app.get('/movies', function (req, res){
     Movies.find()
      .then((movies) => {
         res.status(201).json(movies);
